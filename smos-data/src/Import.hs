767a1f749d403d7b54160900ade9d322cf1681c9
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-10-19 10:10:54 +0200

    Basic reading and writing

--- smos-data/src/Import.hs
+++ smos-data/src/Import.hs
@@ -6,8 +6,16 @@ import Prelude as X hiding (head, init, last, tail)
 
 import GHC.Generics as X
 
+import Data.Maybe as X
+import Data.Monoid as X
 import Data.Validity as X
-import Data.Validity.Containers as X
-import Data.Validity.HashMap as X
-import Data.Validity.Time as X
-import Data.Validity.Text as X
+import Data.Validity.Containers as X ()
+import Data.Validity.HashMap as X ()
+import Data.Validity.Text as X ()
+import Data.Validity.Time as X ()
+
+import Control.Monad as X
+import Control.Monad.IO.Class as X
+
+import Path as X
+import Path.IO as X
--- smos-data/src/Smos/Data.hs
+++ smos-data/src/Smos/Data.hs
@@ -1,4 +1,25 @@
-module Smos.Data (module Smos.Data.Types) where
+module Smos.Data
+    ( module Smos.Data.Types
+    , readSmosFile
+    , writeSmosFile
+    ) where
+
+import Import
+
+import qualified Data.ByteString as SB
+import Data.Yaml as Yaml
 
 import Smos.Data.Types
 
+readSmosFile :: MonadIO m => Path Abs File -> m (Either String SmosFile)
+readSmosFile fp = do
+    mContents <- liftIO $ forgivingAbsence $ SB.readFile $ toFilePath fp
+    case mContents of
+        Nothing -> pure $ Left $ "File not found: " ++ toFilePath fp
+        Just contents -> pure $ Yaml.decodeEither contents
+
+writeSmosFile :: MonadIO m => Path Abs File -> SmosFile -> m ()
+writeSmosFile fp sf =
+    liftIO $ do
+        ensureDir $ parent fp
+        SB.writeFile (toFilePath fp) (Yaml.encode sf)
