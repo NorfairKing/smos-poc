Nickske666/smos

Convert works now

smos-convert/src/Smos/Convert.hs
@@ -6,7 +6,13 @@ module Smos.Convert
 
 import Import
 
+import Smos.Convert.Document
 import Smos.Convert.OptParse
+import Smos.Convert.SmosFile
+
+import Smos.Data
+
+import qualified Data.Text.IO as T
 
 smosConvert :: IO ()
 smosConvert = do
@@ -14,7 +20,13 @@ smosConvert = do
     execute disp sett
 
 execute :: Dispatch -> Settings -> IO ()
-execute (DispatchConvertFile DispatchConvertFileArgs {..}) _ = convert orgpath
+execute (DispatchConvertFile DispatchConvertFileArgs {..}) _ =
+    mapM_ convert orgpaths
 
 convert :: Path Abs File -> IO ()
-convert _ = undefined
+convert path = do
+    text <- T.readFile $ toFilePath path
+    smosPath <- setFileExtension ".smos" path
+    case toSmosFile <$> getDocument text of
+        Left err -> die err
+        Right smosFile -> writeSmosFile smosPath =<< smosFile

smos-convert/src/Smos/Convert/EntryTree.hs
@@ -1,0 +1,48 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Smos.Convert.EntryTree where
+
+import Import
+
+import Smos.Convert.HashMap
+import Smos.Convert.Logbook
+import Smos.Convert.Time
+
+import Smos.Data.Types
+
+import Data.OrgMode.Types
+
+import Data.Time.Clock
+import Data.Time.LocalTime
+
+toEntryTree :: TimeZone -> UTCTime -> Headline -> Tree Entry
+toEntryTree timezone now headline@Headline {..} =
+    let entry =
+            Entry
+            { entryHeader = Header title
+            , entryContents = Just . Contents $ sectionParagraph section
+            , entryTimestamps =
+                  getTimestamps timezone $ sectionPlannings section
+            , entryProperties = getProperties $ sectionProperties section
+            , entryStateHistory = stateHistory timezone now headline
+            , entryTags = Tag <$> tags
+            , entryLogbook = getLogbook timezone $ sectionLogbook section
+            }
+        forrest = toEntryTree timezone now <$> subHeadlines
+    in Node entry forrest
+
+stateHistory :: TimeZone -> UTCTime -> Headline -> StateHistory
+stateHistory timezone now Headline {..} =
+    StateHistory
+        [ StateHistoryEntry
+          { stateHistoryEntryNewState = toState <$> stateKeyword
+          , stateHistoryEntryTimestamp =
+                toUTC timestamp $ toUTC (sectionTimestamp section) now
+          }
+        ]
+  where
+    toUTC maybeStamp stamp2 =
+        fromMaybe stamp2 $ toUTCTime timezone . tsTime <$> maybeStamp
+
+toState :: StateKeyword -> TodoState
+toState (StateKeyword text) = TodoState text

smos-convert/src/Smos/Convert/OptParse.hs
@@ -36,7 +36,8 @@ getSettingsFromConfig _ _ = pure Settings
 
 getDispatch :: Command -> Flags -> IO Dispatch
 getDispatch (ConvertFile ConvertFileArgs {..}) _ =
-    DispatchConvertFile . DispatchConvertFileArgs <$> parseAbsFile orgfile
+    DispatchConvertFile . DispatchConvertFileArgs <$>
+    sequence (parseAbsFile <$> orgfiles)
 
 getConfig :: Flags -> IO Configuration
 getConfig _ = pure Configuration
@@ -68,7 +69,7 @@ parseOrgFilePath =
     strArgument (mconcat [metavar "FILE", help "Orgfile to convert"])
 
 parseCommand :: Parser Command
-parseCommand = ConvertFile . ConvertFileArgs <$> parseOrgFilePath
+parseCommand = ConvertFile . ConvertFileArgs <$> some parseOrgFilePath
 
 parseFlags :: Parser Flags
 parseFlags = pure Flags

smos-convert/src/Smos/Convert/SmosFile.hs
@@ -1,0 +1,25 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Smos.Convert.SmosFile where
+
+import Smos.Convert.EntryTree
+
+import Smos.Data.Types
+
+import Data.OrgMode.Types
+
+import Data.Time.Clock
+import Data.Time.LocalTime
+
+toSmosFile :: Document -> IO SmosFile
+toSmosFile Document {..} = do
+    now <- getCurrentTime
+    timezone <- getCurrentTimeZone
+    let forrest = toEntryTree timezone now <$> documentHeadlines
+    SmosFile <$>
+        case documentText of
+            "" -> pure forrest
+            text ->
+                let tree = Node (newEntry $ Header text) []
+                in pure $ tree : forrest

NorfairKing/smos-poc

smos-convert/src/Smos/Convert.hs
@@ -6,7 +6,13 @@ module Smos.Convert
 
 import Import
 
+import Smos.Convert.Document
 import Smos.Convert.OptParse
+import Smos.Convert.SmosFile
+
+import Smos.Data
+
+import qualified Data.Text.IO as T
 
 smosConvert :: IO ()
 smosConvert = do
@@ -14,7 +20,13 @@ smosConvert = do
     execute disp sett
 
 execute :: Dispatch -> Settings -> IO ()
-execute (DispatchConvertFile DispatchConvertFileArgs {..}) _ = convert orgpath
+execute (DispatchConvertFile DispatchConvertFileArgs {..}) _ =
+    mapM_ convert dispatchConvertPaths
 
 convert :: Path Abs File -> IO ()
-convert _ = undefined
+convert path = do
+    text <- T.readFile $ toFilePath path
+    smosPath <- setFileExtension ".smos" path
+    case toSmosFile <$> getDocument text of
+        Left err -> die err
+        Right smosFile -> writeSmosFile smosPath =<< smosFile

smos-convert/src/Smos/Convert/EntryTree.hs
@@ -1,0 +1,48 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Smos.Convert.EntryTree where
+
+import Import
+
+import Smos.Convert.HashMap
+import Smos.Convert.Logbook
+import Smos.Convert.Time
+
+import Smos.Data.Types
+
+import Data.OrgMode.Types
+
+import Data.Time.Clock
+import Data.Time.LocalTime
+
+toEntryTree :: TimeZone -> UTCTime -> Headline -> Tree Entry
+toEntryTree timezone now headline@Headline {..} =
+    let entry =
+            Entry
+            { entryHeader = Header title
+            , entryContents = Just . Contents $ sectionParagraph section
+            , entryTimestamps =
+                  getTimestamps timezone $ sectionPlannings section
+            , entryProperties = getProperties $ sectionProperties section
+            , entryStateHistory = stateHistory timezone now headline
+            , entryTags = Tag <$> tags
+            , entryLogbook = getLogbook timezone $ sectionLogbook section
+            }
+        forest = toEntryTree timezone now <$> subHeadlines
+    in Node entry forest
+
+stateHistory :: TimeZone -> UTCTime -> Headline -> StateHistory
+stateHistory timezone now Headline {..} =
+    StateHistory
+        [ StateHistoryEntry
+          { stateHistoryEntryNewState = toState <$> stateKeyword
+          , stateHistoryEntryTimestamp =
+                toUTC timestamp $ toUTC (sectionTimestamp section) now
+          }
+        ]
+  where
+    toUTC maybeStamp stamp2 =
+        fromMaybe stamp2 $ toUTCTime timezone . tsTime <$> maybeStamp
+
+toState :: StateKeyword -> TodoState
+toState (StateKeyword text) = TodoState text

smos-convert/src/Smos/Convert/OptParse.hs
@@ -36,7 +36,8 @@ getSettingsFromConfig _ _ = pure Settings
 
 getDispatch :: Command -> Flags -> IO Dispatch
 getDispatch (ConvertFile ConvertFileArgs {..}) _ =
-    DispatchConvertFile . DispatchConvertFileArgs <$> parseAbsFile orgfile
+    DispatchConvertFile . DispatchConvertFileArgs <$>
+    sequence (parseAbsFile <$> convertArgsFiles)
 
 getConfig :: Flags -> IO Configuration
 getConfig _ = pure Configuration
@@ -68,7 +69,7 @@ parseOrgFilePath =
     strArgument (mconcat [metavar "FILE", help "Orgfile to convert"])
 
 parseCommand :: Parser Command
-parseCommand = ConvertFile . ConvertFileArgs <$> parseOrgFilePath
+parseCommand = ConvertFile . ConvertFileArgs <$> some parseOrgFilePath
 
 parseFlags :: Parser Flags
 parseFlags = pure Flags

smos-convert/src/Smos/Convert/SmosFile.hs
@@ -1,0 +1,25 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Smos.Convert.SmosFile where
+
+import Smos.Convert.EntryTree
+
+import Smos.Data.Types
+
+import Data.OrgMode.Types
+
+import Data.Time.Clock
+import Data.Time.LocalTime
+
+toSmosFile :: Document -> IO SmosFile
+toSmosFile Document {..} = do
+    now <- getCurrentTime
+    timezone <- getCurrentTimeZone
+    let forest = toEntryTree timezone now <$> documentHeadlines
+    SmosFile <$>
+        case documentText of
+            "" -> pure forest
+            text ->
+                let tree = Node (newEntry $ Header text) []
+                in pure $ tree : forest